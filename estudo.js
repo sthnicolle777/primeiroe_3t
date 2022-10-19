2d285590f8f0b7eceea1830ef2699695aff91062
sthnicolle777 <110102474+sthnicolle777@users.noreply.github.com>
2022-10-19 09:08:45 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -39,3 +39,11 @@ function calcula(){
     let res =  val * (1+(j/100)); 
     document.write("Resultado +" +res);
 }
+
+function somaNota(){
+    let n1 = document.getElementById("t1").value
+    let n2 = document.getElementById("t2").value
+    let n3 = document.getElementById("t3").value
+    let r = Number(n1) + Number(n2) + Number(n3)
+    document.getElementById("resultado").innerHTML = "Soma: " + r;
+}
