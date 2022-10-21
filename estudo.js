130b6c785e69062ad2b5f61ff034ed18f33e83d7
sthnicolle777 <110102474+sthnicolle777@users.noreply.github.com>
2022-10-20 21:44:37 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,49 +1,89 @@
-let tabuada = 2;
+let tabuada = 9;
 
-function escreva() {
-    document.write( "Tabuada do " + tabuada + "<br>")
-    document.write(tabuada + " x 1 =" + (tabuada*1) +"<br>");
-    document.write(tabuada + " x 2 =" + (tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 =" + (tabuada*3) +"<br>");
-    document.write(tabuada + " x 4 =" + (tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 =" + (tabuada*5) +"<br>");
-    document.write(tabuada + " x 6 =" + (tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 =" + (tabuada*7) +"<br>");
-    document.write(tabuada + " x 8 =" + (tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 =" + (tabuada*9) +"<br>");
-    document.write(tabuada + " x 10 =" + (tabuada*10)+"<br>");
+function escreva(){
+    document.write("<h1>Tabuada do " + tabuada + "</h1>");
+    document.write(tabuada + "x 1 = " + (tabuada*1)+"<br>");
+    document.write(tabuada + "x 2 = " + (tabuada*2)+"<br>");
+    document.write(tabuada + "x 3 = " + (tabuada*3)+"<br>");
+    document.write(tabuada + "x 4 = " + (tabuada*4)+"<br>");
+    document.write(tabuada + "x 5 = " + (tabuada*5)+"<br>");
+    document.write(tabuada + "x 6 = " + (tabuada*6)+"<br>");
+    document.write(tabuada + "x 7 = " + (tabuada*7)+"<br>");
+    document.write(tabuada + "x 8 = " + (tabuada*8)+"<br>");
+    document.write(tabuada + "x 9 = " + (tabuada*9)+"<br>");
+    document.write(tabuada + "x 10 = " + (tabuada*10)+"<br>");
 }
 
-function minhaTabuada() {
-    for(let i=0; i < 10; i++){
-        document.write("Tabuada do" + 1 + "<br>");
+function minhaTabuada(){
+    for(let i=1; i <= 10; i++){
+        document.write("Tabuada do " + i + "<br>");
         for(let j = 1; j <= 10; j++){
-           document.write(tabuada + " x " + j + " = " + (j*i)+ "<br>");
-    }
-    document.write("<br>");
-}
-}
-
-function quadrado() {
-    for(let i=1; 1 <= 5; i++){
-        document.write("O quadrado de" + i + "é" + (i*i) + "<br>");
-
+            document.write(i + " x " + j + " = " + (j*i)+"<br>");
+        }
+        document.write("<br>");
     }
 }
 
+function quadrado(){
+    for(let i = 2; i <= 1000; i++){
+            document.write("O quadrado de " + i + " é " + (i*i) + "<br>");
+    }
+}
 
 function calcula(){
     let val = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
-    let res =  val * (1+(j/100)); 
-    document.write("Resultado +" +res);
+    if(!Number(val)){
+        alert("O valor deve ser um número.");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor").focus();
+        return
+    }
+    if(!Number(j)){
+        alert("O valor deve ser um número.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return
+    }
+    if(!Number(t)){
+        alert("O valor deve ser um número.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return
+    }
+    let res = val * (1+(j/100));
+    for(let m=1; m <= t;m++){
+        res = val * (1+(j/100));
+        val = res;
+        document.write("Mẽs " + m + " = " + res + "<br>")
+    }
+    
+    document.write("Resultado: "+res);
 }
 
-function somaNota(){
-    let n1 = document.getElementById("t1").value
-    let n2 = document.getElementById("t2").value
-    let n3 = document.getElementById("t3").value
-    let r = Number(n1) + Number(n2) + Number(n3)
-    document.getElementById("resultado").innerHTML = "Soma: " + r;
+function somarNotas(){
+    let n1 = document.getElementById("t1").value;
+    let n2 = document.getElementById("t2").value;
+    let n3 = document.getElementById("t3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    document.getElementById("resultadosoma").innerHTML = "Soma: " + r;
+}
+
+function mediaNotas(){
+    let n1 = document.getElementById("t1").value;
+    let n2 = document.getElementById("t2").value;
+    let n3 = document.getElementById("t3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    r = r / Number(3)
+    document.getElementById("resultadomedia").innerHTML = "Média: " + r;
+}
+
+function fecharNotas(){
+    let n1 = document.getElementById("t1").value;
+    let n2 = document.getElementById("t2").value;
+    let n3 = document.getElementById("t3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    r = 180 - r;
+    document.getElementById("resultadofechar").innerHTML = "Falta: " + r;
 }
